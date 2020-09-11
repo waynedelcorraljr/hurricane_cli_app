33e8a3b13b59138dffbc77eef517c0374fba9b54
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-09-11 02:55:59 -0500

    updates flowchart, gives CLI pizzazz

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,54 +1,93 @@
 require 'pry'
+require 'colorize'
 class CommandLineInterface
 
     def main
         greet_user
-        provide_paths
-        capture_path        
+        create_objects
+        capture_path_loop        
     end
 
     def greet_user
-        puts 'Hello, welcome to the Hurricane CLI App!'
+        puts '*****************************************************************'.yellow
+        puts '*                                                               *'.yellow
+        puts '*           Hello, welcome to the Hurricane CLI App!            *'.yellow
+        puts '*                                                               *'.yellow
+        puts '*****************************************************************'.yellow
     end
 
-    def provide_paths
+    def create_objects
         storms = Scraper.scrape_storms_and_details
         hashed_storms = storms.map {|key, value| {name: key, details: value}}
         Storm.create_storms_from_collection(hashed_storms) 
         regions = Scraper.scrape_regions_and_activity
         Region.create_regions_from_collection(regions)
-        puts 'To view all active storms, enter "active storms"'
-        puts 'To view details of all active storms, enter "active storm details"'
-        puts 'To view region activity, enter "region activity"'
-        puts 'To exit the application, enter "exit"'
 
     end
-    def capture_path
+    def capture_path_loop
         input = " "
-        puts "What would you like to see?"
+        puts ''
+        puts '*****************************************************************'.yellow
+        puts '*                                                               *'.yellow
+        puts '*           To view all active storms, enter "active"           *'.yellow
+        puts '*         To view details of all storms, enter "details"        *'.yellow
+        puts '*            To view region activity, enter "regions"           *'.yellow
+        puts '*               For full report, enter "full report"            *'.yellow
+        puts '*              To exit the application, enter "exit"            *'.yellow          
+        puts '*                                                               *'.yellow
+        puts '*****************************************************************'.yellow
+        puts ''
+        puts "Your wish is my command..."
+        puts ''
         input = gets.strip
-            if input == "active storm details"
-                storm_list = Storm.all.map {|storm| {storm.name => storm.details}}
-                puts storm_list
-                capture_path
-            elsif input == "active storms"
-                storm_list = Storm.all.map {|storm| storm.name}
-                puts storm_list
-                capture_path
-            elsif input == "region activity"
-                region_list = Region.all.map {|region| {region.name => region.number_storms_active}}
-                puts region_list
-                capture_path
+            if input == "details"
+                Storm.all.each do |storm| 
+                    puts ""
+                    puts "#{storm.name}".blue
+                    puts "#{storm.details}".green
+                end
+                capture_path_loop
+            elsif input == "active"
+                Storm.all.each do |storm| 
+                    puts ""
+                    puts "#{storm.name}".blue
+                end
+                capture_path_loop
+            elsif input == "regions"
+                Region.all.each do |region| 
+                    puts ""
+                    puts "#{region.name}".blue
+                    puts "#{region.number_storms_active}".green
+                end
+                capture_path_loop
+            elsif input == "full report"
+                Region.all.each do |region| 
+                    puts ""
+                    puts "#{region.name}".blue
+                    puts "#{region.number_storms_active}".green
+                end
+                puts ""
+                puts "Storms are listed in the above Region order (Atlantic -> Indian)".red
+                Storm.all.each do |storm| 
+                    puts ""
+                    puts "#{storm.name}".blue
+                    puts "#{storm.details}".green
+                end
+                capture_path_loop
             elsif input == "exit"
                 say_goodbye
             else
                 puts "Invalid input, please try again."
-                capture_path
+                capture_path_loop
             end
     end
 
     def say_goodbye
-        puts "Thanks for using the Hurricane CLI App!"
+        puts '*****************************************************************'.yellow
+        puts '*                                                               *'.yellow
+        puts '*           Thanks for using the Hurricane CLI App!             *'.yellow
+        puts '*                                                               *'.yellow
+        puts '*****************************************************************'.yellow
         exit
     end
 end
